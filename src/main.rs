738e85389d1092847d178d6b89b5cf6abba9a612
Alan Sparrow <git@freeflight.org.uk>
2024-01-15 12:09:55 +0000

    Add YAIXM

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,52 @@
-use yew::{function_component, html, Html};
+use gloo_net::http::Request;
+use gloo_net::Error;
+use yew::{function_component, html, use_effect_with, use_state, Html};
+
+use yaixm::Yaixm;
+
+mod yaixm;
 
 #[function_component]
 fn App() -> Html {
-    html! {
-        <p>{ "Hello World" }</p>
+    let yaixm = use_state(|| None);
+
+    // Fetch YAIXM data
+    {
+        let yaixm = yaixm.clone();
+
+        // use_effect_with((), ...) triggers only on first render of component
+        use_effect_with((), move |_| {
+            wasm_bindgen_futures::spawn_local(async move {
+                let data = fetch_yaixm().await;
+                yaixm.set(data.ok());
+            });
+            || ()
+        });
+    }
+
+    // HTML rendering
+    match yaixm.as_ref() {
+        // Render full interface if YAIXM data is available
+        Some(yaixm) => {
+            html! {
+                yaixm.release.airac_date.as_str()
+            }
+        }
+
+        None => {
+            html! {
+                { "No YAIXM" }
+            }
+        }
+    }
+}
+
+// Get YAIXM data from server
+async fn fetch_yaixm() -> Result<Yaixm, Error> {
+    let result = Request::get("yaixm.json").send().await;
+    match result {
+        Ok(response) => response.json().await,
+        Err(e) => Err(e),
     }
 }
 
---
+++ src/yaixm.rs
@@ -0,0 +1,188 @@
+use serde::Deserialize;
+
+#[derive(Clone, Copy, Deserialize, Debug, Eq, PartialEq)]
+pub enum IcaoClass {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+}
+
+#[derive(Clone, Copy, Deserialize, Eq, PartialEq, Debug)]
+pub enum IcaoType {
+    #[serde(rename = "ATZ")]
+    Atz,
+    #[serde(rename = "AWY")]
+    Awy,
+    #[serde(rename = "CTA")]
+    Cta,
+    #[serde(rename = "CTR")]
+    Ctr,
+    D,
+    #[serde(rename = "D_OTHER")]
+    DOther,
+    #[serde(rename = "OTHER")]
+    Other,
+    P,
+    R,
+    #[serde(rename = "TMA")]
+    Tma,
+}
+
+#[derive(Clone, Copy, Deserialize, Eq, PartialEq, Debug)]
+pub enum LocalType {
+    #[serde(rename = "DZ")]
+    Dz,
+    #[serde(rename = "GLIDER")]
+    Glider,
+    #[serde(rename = "GVS")]
+    Gvs,
+    #[serde(rename = "HIRTA")]
+    Hirta,
+    #[serde(rename = "ILS")]
+    Ils,
+    #[serde(rename = "LASER")]
+    Laser,
+    #[serde(rename = "MATZ")]
+    Matz,
+    #[serde(rename = "NOATZ")]
+    NoAtz,
+    #[serde(rename = "RAT")]
+    Rat,
+    #[serde(rename = "RMZ")]
+    Rmz,
+    #[serde(rename = "UL")]
+    Ul,
+    #[serde(rename = "TMZ")]
+    Tmz,
+}
+
+#[derive(Clone, Copy, Deserialize, Debug, Eq, Hash, PartialEq)]
+pub enum Rule {
+    #[serde(rename = "INTENSE")]
+    Intense,
+    #[serde(rename = "LOA")]
+    Loa,
+    #[serde(rename = "NOSSR")]
+    NoSsr,
+    #[serde(rename = "NOTAM")]
+    Notam,
+    #[serde(rename = "RAZ")]
+    Raz,
+    #[serde(rename = "RMZ")]
+    Rmz,
+    #[serde(rename = "SI")]
+    Si,
+    #[serde(rename = "TRA")]
+    Tra,
+    #[serde(rename = "TMZ")]
+    Tmz,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Circle {
+    pub centre: String,
+    pub radius: String,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Arc {
+    pub centre: String,
+    pub dir: String,
+    pub radius: String,
+    pub to: String,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub enum Boundary {
+    #[serde(rename = "circle")]
+    Circle(Circle),
+    #[serde(rename = "arc")]
+    Arc(Arc),
+    #[serde(rename = "line")]
+    Line(Vec<String>),
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Volume {
+    pub id: Option<String>,
+    pub name: Option<String>,
+    pub lower: String,
+    pub upper: String,
+    #[serde(rename = "class")]
+    pub icao_class: Option<IcaoClass>,
+    pub rules: Option<Vec<Rule>>,
+    pub seq: Option<String>,
+    pub frequency: Option<f64>,
+    pub boundary: Vec<Boundary>,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Feature {
+    pub id: Option<String>,
+    pub name: String,
+    #[serde(rename = "type")]
+    pub icao_type: IcaoType,
+    #[serde(rename = "localtype")]
+    pub local_type: Option<LocalType>,
+    #[serde(rename = "class")]
+    pub icao_class: Option<IcaoClass>,
+    pub rules: Option<Vec<Rule>>,
+    pub geometry: Vec<Volume>,
+}
+
+#[derive(Clone, Deserialize, Debug)]
+pub struct Replace {
+    pub id: String,
+    pub geometry: Vec<Volume>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct LoaArea {
+    pub name: String,
+    pub add: Vec<Feature>,
+    pub replace: Option<Vec<Replace>>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Loa {
+    pub name: String,
+    pub default: Option<bool>,
+    pub areas: Vec<LoaArea>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Obstacle {
+    pub elevation: String,
+    pub name: String,
+    pub position: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Service {
+    pub callsign: String,
+    pub frequency: f64,
+    pub controls: Vec<String>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Release {
+    pub airac_date: String,
+    pub timestamp: String,
+    pub schema_version: u8,
+    pub note: String,
+    pub commit: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Yaixm {
+    pub airspace: Vec<Feature>,
+    pub rat: Vec<Feature>,
+    pub loa: Vec<Loa>,
+    pub obstacle: Vec<Obstacle>,
+    pub service: Vec<Service>,
+    pub release: Release,
+}
