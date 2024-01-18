1b748c82cf2029ade9436388d5db236ee82d7e6e
Alan Sparrow <git@freeflight.org.uk>
2024-01-18 17:24:59 +0000

    Associate labels with controls

--- src/components/airspace_tab.rs
+++ src/components/airspace_tab.rs
@@ -39,62 +39,34 @@ pub fn airspace_tab(props: &Props) -> Html {
           <div class="columns">
             <div class="column is-one-third">
               <div class="field">
-                <label class="label">{"ATZ"}</label>
-                <div class="control">
-                  <div class="select is-fullwidth">
-                    <select name="atz" onchange={onchange.clone()}>
-                      <option value="classd" selected={set.atz == AirType::ClassD}>{"Class D"}</option>
-                      <option value="ctr" selected={set.atz == AirType::Ctr}>{"Control Zone"}</option>
-                    </select>
+                <label class="label">
+                  {"ATZ"}
+                  <div class="control">
+                    <div class="select is-fullwidth">
+                      <select name="atz" onchange={onchange.clone()}>
+                        <option value="classd" selected={set.atz == AirType::ClassD}>{"Class D"}</option>
+                        <option value="ctr" selected={set.atz == AirType::Ctr}>{"Control Zone"}</option>
+                      </select>
+                    </div>
                   </div>
-                </div>
+                </label>
               </div>
             </div>
 
             <div class="column is-one-third">
               <div class="field">
-                <label class="label">{"ILS Feather"}</label>
-                <div class="control">
-                  <div class="select is-fullwidth">
-                    <select name="ils" onchange={onchange.clone()}>
-                      <option value="atz" selected={set.ils.is_none()}>{"As ATZ"}</option>
-                      <option value="classf" selected={set.ils == Some(AirType::ClassF)}>{"Class F"}</option>
-                      <option value="classg" selected={set.ils == Some(AirType::ClassG)}>{"Class G"}</option>
-                    </select>
+                <label class="label">
+                  {"ILS Feather"}
+                  <div class="control">
+                    <div class="select is-fullwidth">
+                      <select name="ils" onchange={onchange.clone()}>
+                        <option value="atz" selected={set.ils.is_none()}>{"As ATZ"}</option>
+                        <option value="classf" selected={set.ils == Some(AirType::ClassF)}>{"Class F"}</option>
+                        <option value="classg" selected={set.ils == Some(AirType::ClassG)}>{"Class G"}</option>
+                      </select>
+                    </div>
                   </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="columns">
-            <div class="column is-one-third">
-              <div class="field">
-                <label class="label">{"Non-ATZ Airfield"}</label>
-                <div class="control">
-                  <div class="select is-fullwidth">
-                    <select name="unlicensed" onchange={onchange.clone()}>
-                      <option value="exclude" selected={set.unlicensed.is_none()}>{"No"}</option>
-                      <option value="classf" selected={set.unlicensed == Some(AirType::ClassF)}>{"Class F"}</option>
-                      <option value="classg" selected={set.unlicensed == Some(AirType::ClassG)}>{"Class G"}</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="column is-one-third">
-              <div class="field">
-                <label class="label">{"Microlight Airfield"}</label>
-                <div class="control">
-                  <div class="select is-fullwidth">
-                    <select name="microlight" onchange={onchange.clone()}>
-                      <option value="exclude" selected={set.microlight.is_none()}>{"No"}</option>
-                      <option value="classf" selected={set.microlight == Some(AirType::ClassF)}>{"Class F"}</option>
-                      <option value="classg" selected={set.microlight == Some(AirType::ClassG)}>{"Class G"}</option>
-                    </select>
-                  </div>
-                </div>
+                </label>
               </div>
             </div>
           </div>
@@ -102,31 +74,71 @@ pub fn airspace_tab(props: &Props) -> Html {
           <div class="columns">
             <div class="column is-one-third">
               <div class="field">
-                <label class="label">{"Gliding Airfield"}</label>
-                <div class="control">
-                  <div class="select is-fullwidth">
-                    <select name="gliding" onchange={onchange.clone()}>
-                      <option value="exclude" selected={set.gliding.is_none()}>{"No"}</option>
-                      <option value="gsec" selected={set.gliding == Some(AirType::Gliding)}>{"Gliding Sector"}</option>
-                      <option value="classf" selected={set.gliding == Some(AirType::ClassF)}>{"Class F"}</option>
-                      <option value="classg" selected={set.gliding == Some(AirType::ClassG)}>{"Class G"}</option>
-                    </select>
+                <label class="label">
+                  {"Non-ATZ Airfield"}
+                  <div class="control">
+                    <div class="select is-fullwidth">
+                      <select name="unlicensed" onchange={onchange.clone()}>
+                        <option value="exclude" selected={set.unlicensed.is_none()}>{"No"}</option>
+                        <option value="classf" selected={set.unlicensed == Some(AirType::ClassF)}>{"Class F"}</option>
+                        <option value="classg" selected={set.unlicensed == Some(AirType::ClassG)}>{"Class G"}</option>
+                      </select>
+                    </div>
                   </div>
-                </div>
+                </label>
               </div>
             </div>
 
             <div class="column is-one-third">
               <div class="field">
-                <label class="label">{"Exclude Home Airfield"}</label>
-                <div class="control">
-                  <div class="select is-fullwidth">
-                    <select name="home" onchange={onchange.clone()}>
-                      <option selected={set.home.is_none()}>{"No"}</option>
-                      { gliding_sites() }
-                    </select>
+                <label class="label">
+                  {"Microlight Airfield"}
+                  <div class="control">
+                    <div class="select is-fullwidth">
+                      <select name="microlight" onchange={onchange.clone()}>
+                        <option value="exclude" selected={set.microlight.is_none()}>{"No"}</option>
+                        <option value="classf" selected={set.microlight == Some(AirType::ClassF)}>{"Class F"}</option>
+                        <option value="classg" selected={set.microlight == Some(AirType::ClassG)}>{"Class G"}</option>
+                      </select>
+                    </div>
                   </div>
-                </div>
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div class="columns">
+            <div class="column is-one-third">
+              <div class="field">
+                <label class="label">
+                  {"Gliding Airfield"}
+                  <div class="control">
+                    <div class="select is-fullwidth">
+                      <select name="gliding" onchange={onchange.clone()}>
+                        <option value="exclude" selected={set.gliding.is_none()}>{"No"}</option>
+                        <option value="gsec" selected={set.gliding == Some(AirType::Gliding)}>{"Gliding Sector"}</option>
+                        <option value="classf" selected={set.gliding == Some(AirType::ClassF)}>{"Class F"}</option>
+                        <option value="classg" selected={set.gliding == Some(AirType::ClassG)}>{"Class G"}</option>
+                      </select>
+                    </div>
+                  </div>
+                </label>
+              </div>
+            </div>
+
+            <div class="column is-one-third">
+              <div class="field">
+                <label class="label">
+                  {"Exclude Home Airfield"}
+                  <div class="control">
+                    <div class="select is-fullwidth">
+                      <select name="home" onchange={onchange.clone()}>
+                        <option selected={set.home.is_none()}>{"No"}</option>
+                        { gliding_sites() }
+                      </select>
+                    </div>
+                  </div>
+                </label>
               </div>
             </div>
           </div>
--- src/components/options_tab.rs
+++ src/components/options_tab.rs
@@ -25,66 +25,37 @@ pub fn options_tab(props: &Props) -> Html {
           <div class="columns">
             <div class="column is-one-third">
               <div class="field">
-                <label class="label">{"Format"}</label>
-                <div class="control">
-                  <div class="select is-fullwidth">
-                    <select name="format" onchange={onchange.clone()}>
-                      <option value="openair" selected={set.format == Format::OpenAir}>{"OpenAir"}</option>
-                      <option value="ratonly" selected={set.format == Format::RatOnly}>{"RA(T) only"}</option>
-                      <option value="competition" selected={set.format == Format::Competition}>{"Competition"}</option>
-                    </select>
+                <label class="label">
+                  {"Format"}
+                  <div class="control">
+                    <div class="select is-fullwidth">
+                      <select name="format" onchange={onchange.clone()}>
+                        <option value="openair" selected={set.format == Format::OpenAir}>{"OpenAir"}</option>
+                        <option value="ratonly" selected={set.format == Format::RatOnly}>{"RA(T) only"}</option>
+                        <option value="competition" selected={set.format == Format::Competition}>{"Competition"}</option>
+                      </select>
+                    </div>
                   </div>
-                </div>
+                </label>
               </div>
             </div>
 
             <div class="column is-one-third">
               <div class="field">
-                <label class="label">{"Maximum Level"}</label>
-                <div class="control">
-                  <div class="select is-fullwidth">
-                    <select name="max_level" onchange={onchange.clone()}>
-                      <option value="600" selected={set.max_level == 600}>{"Unlimited"}</option>
-                      <option value="195" selected={set.max_level == 195}>{"FL195"}</option>
-                      <option value="125" selected={set.max_level == 125}>{"FL125"}</option>
-                      <option value="105" selected={set.max_level == 105}>{"FL105"}</option>
-                      <option value="65" selected={set.max_level == 65}>{"FL65"}</option>
-                    </select>
+                <label class="label">
+                  {"Maximum Level"}
+                  <div class="control">
+                    <div class="select is-fullwidth">
+                      <select name="max_level" onchange={onchange.clone()}>
+                        <option value="600" selected={set.max_level == 600}>{"Unlimited"}</option>
+                        <option value="195" selected={set.max_level == 195}>{"FL195"}</option>
+                        <option value="125" selected={set.max_level == 125}>{"FL125"}</option>
+                        <option value="105" selected={set.max_level == 105}>{"FL105"}</option>
+                        <option value="65" selected={set.max_level == 65}>{"FL65"}</option>
+                      </select>
+                    </div>
                   </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="columns">
-            <div class="column is-one-third">
-              <div class="field">
-                <label class="label">{"HIRTA/GVS"}</label>
-                <div class="control">
-                  <div class="select is-fullwidth">
-                    <select name="hirta_gvs" onchange={onchange.clone()}>
-                      <option value="exclude" selected={set.hirta_gvs.is_none()}>{"No"}</option>
-                      <option value="danger" selected={set.hirta_gvs == Some(AirType::Danger)}>{"Danger"}</option>
-                      <option value="restricted" selected={set.hirta_gvs == Some(AirType::Restricted)}>{"Restricted"}</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="column is-one-third">
-              <div class="field">
-                <label class="label">{"Obstacle"}</label>
-                <div class="control">
-                  <div class="select is-fullwidth">
-                    <select name="obstacle" onchange={onchange.clone()}>
-                      <option value="exclude" selected={set.obstacle.is_none()}>{"No"}</option>
-                      <option value="danger" selected={set.obstacle == Some(AirType::Danger)}>{"Danger"}</option>
-                      <option value="classf" selected={set.obstacle == Some(AirType::ClassF)}>{"Class F"}</option>
-                      <option value="classg" selected={set.obstacle == Some(AirType::ClassG)}>{"Class G"}</option>
-                    </select>
-                  </div>
-                </div>
+                </label>
               </div>
             </div>
           </div>
@@ -92,31 +63,72 @@ pub fn options_tab(props: &Props) -> Html {
           <div class="columns">
             <div class="column is-one-third">
               <div class="field">
-                <label class="label">{"Radio Frequency"}</label>
-                <div class="control">
-                  <div class="select is-fullwidth">
-                    <select name="radio" onchange={onchange.clone()}>
-                      <option value="no" selected={!set.radio}>{"No"}</option>
-                      <option value="yes" selected={set.radio}>{"Append to name"}</option>
-                    </select>
+                <label class="label">
+                  {"HIRTA/GVS"}
+                  <div class="control">
+                    <div class="select is-fullwidth">
+                      <select name="hirta_gvs" onchange={onchange.clone()}>
+                        <option value="exclude" selected={set.hirta_gvs.is_none()}>{"No"}</option>
+                        <option value="danger" selected={set.hirta_gvs == Some(AirType::Danger)}>{"Danger"}</option>
+                        <option value="restricted" selected={set.hirta_gvs == Some(AirType::Restricted)}>{"Restricted"}</option>
+                      </select>
+                    </div>
                   </div>
-                </div>
+                </label>
               </div>
             </div>
 
             <div class="column is-one-third">
               <div class="field">
-                <label class="label">{"Altitude Overlay"}</label>
-                <div class="control">
-                  <div class="select is-fullwidth">
-                    <select name="overlay" onchange={onchange.clone()}>
-                      <option value="no" selected={set.overlay.is_none()}>{"No"}</option>
-                      <option value="fl105" selected={set.overlay == Some(Overlay::FL105)}>{"Bases to FL105"}</option>
-                      <option value="fl65" selected={set.overlay == Some(Overlay::FL65)}>{"Bases to FL65"}</option>
-                      <option value="atzdz" selected={set.overlay == Some(Overlay::AtzDz)}>{"Bases to FL105 and ATZ/DZ"}</option>
-                    </select>
+                <label class="label">
+                  {"Obstacle"}
+                  <div class="control">
+                    <div class="select is-fullwidth">
+                      <select name="obstacle" onchange={onchange.clone()}>
+                        <option value="exclude" selected={set.obstacle.is_none()}>{"No"}</option>
+                        <option value="danger" selected={set.obstacle == Some(AirType::Danger)}>{"Danger"}</option>
+                        <option value="classf" selected={set.obstacle == Some(AirType::ClassF)}>{"Class F"}</option>
+                        <option value="classg" selected={set.obstacle == Some(AirType::ClassG)}>{"Class G"}</option>
+                      </select>
+                    </div>
                   </div>
-                </div>
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div class="columns">
+            <div class="column is-one-third">
+              <div class="field">
+                <label class="label">
+                  {"Radio Frequency"}
+                  <div class="control">
+                    <div class="select is-fullwidth">
+                      <select name="radio" onchange={onchange.clone()}>
+                        <option value="no" selected={!set.radio}>{"No"}</option>
+                        <option value="yes" selected={set.radio}>{"Append to name"}</option>
+                      </select>
+                    </div>
+                  </div>
+                </label>
+              </div>
+            </div>
+
+            <div class="column is-one-third">
+              <div class="field">
+                <label class="label">
+                  {"Altitude Overlay"}
+                  <div class="control">
+                    <div class="select is-fullwidth">
+                      <select name="overlay" onchange={onchange.clone()}>
+                        <option value="no" selected={set.overlay.is_none()}>{"No"}</option>
+                        <option value="fl105" selected={set.overlay == Some(Overlay::FL105)}>{"Bases to FL105"}</option>
+                        <option value="fl65" selected={set.overlay == Some(Overlay::FL65)}>{"Bases to FL65"}</option>
+                        <option value="atzdz" selected={set.overlay == Some(Overlay::AtzDz)}>{"Bases to FL105 and ATZ/DZ"}</option>
+                      </select>
+                    </div>
+                  </div>
+                </label>
               </div>
             </div>
           </div>
