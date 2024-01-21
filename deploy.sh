2aebf08a1f978019504d98cfe315a29ea23ced26
Alan Sparrow <git@freeflight.org.uk>
2024-01-21 13:46:18 +0000

    Add comment

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 mkdir _tmp
 cd _tmp
 
-# Clone gh-pages branch and copy new files
+# Clone gh-pages branch and sync new files
 git clone -b gh-pages git@github.com:ahsparrow/asselect3
 rsync -r --delete --filter "P .*" --filter "P CNAME" ../dist/ asselect3
 
